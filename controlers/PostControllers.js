3728cf0750de8f4b4717c9672892424e7a2d77c5
DmPe91 <dmper67@gmail.com>
2023-05-29 21:57:22 +0300

    работа с изображением

--- controlers/PostControllers.js
+++ controlers/PostControllers.js
@@ -1,5 +1,22 @@
 import PostModel from "../models/post.js";
 
+export const getLastTags = async (req, res) => {
+  try {
+    const posts = await PostModel.find().limit(5).exec();
+
+    const tags = posts
+      .map((obj) => obj.tags)
+      .flat()
+      .slice(0, 5);
+    res.json(tags);
+  } catch (err) {
+    console.log(err);
+    res.status(500).json({
+      message: "Не удалось получить статьи",
+    });
+  }
+};
+
 export const getAll = async (req, res) => {
   try {
     const posts = await PostModel.find().populate("user").exec();
@@ -15,7 +32,7 @@ export const getAll = async (req, res) => {
 export const getOne = async (req, res) => {
   try {
     const postId = req.params.id;
-    PostModel.findOneAndUpdate(
+    const doc = await PostModel.findOneAndUpdate(
       {
         _id: postId,
       },
@@ -25,21 +42,13 @@ export const getOne = async (req, res) => {
       {
         returnDocument: "after",
       }
-    ).then((doc, err) => {
-      if (!doc) {
-        return res.status(404).json({
-          message: "Статья не найдена",
-        });
-      }
-
-      if (err) {
-        console.log(err);
-        return res.status(500).json({
-          message: "Не удалось вернуть статью",
-        });
-      }
-      res.json(doc);
-    });
+    ).populate("user");
+    if (!doc) {
+      return res.status(404).json({
+        message: "Статья не найдена",
+      });
+    }
+    res.json(doc);
   } catch (err) {
     console.log(err);
     res.status(500).json({
@@ -83,7 +92,7 @@ export const create = async (req, res) => {
       title: req.body.title,
       text: req.body.text,
       imageUrl: req.body.imageUrl,
-      tags: req.body.tags,
+      tags: req.body.tags.split(","),
       user: req.userId,
     });
 
--- index.js
+++ index.js
@@ -60,7 +60,9 @@ app.post(
   handleValidationsErrors,
   PostControllers.create
 );
+app.get("/tags", PostControllers.getLastTags);
 app.get("/posts", PostControllers.getAll);
+app.get("posts/tags", PostControllers.getLastTags);
 app.get("/posts/:id", PostControllers.getOne);
 app.delete("/posts/:id", checkAuth, PostControllers.remove);
 app.patch(
